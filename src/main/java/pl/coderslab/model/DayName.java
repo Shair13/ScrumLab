414afec429ed7f961c002be3af5de2cebe6d6c63
Szer13 <shad350@gmail.com>
2023-08-28 21:59:22 +0200

    removed id from constructor

--- src/main/java/pl/coderslab/model/DayName.java
+++ src/main/java/pl/coderslab/model/DayName.java
@@ -8,8 +8,7 @@ public class DayName {
     public DayName() {
     }
 
-    public DayName(int id, String name, int displayOrder) {
-        this.id = id;
+    public DayName(String name, int displayOrder) {
         this.name = name;
         this.displayOrder = displayOrder;
     }
