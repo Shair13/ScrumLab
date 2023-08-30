8944e0c0e8573217d58d3a12e08664a4065a2734
MacApos <maciejapostol98@gmail.com>
2023-08-30 18:59:32 +0200

    3-AdminDao-autoryzacja

--- src/main/java/pl/coderslab/dao/AdminDao.java
+++ src/main/java/pl/coderslab/dao/AdminDao.java
@@ -17,6 +17,8 @@ public class AdminDao {
     private static final String DELETE_ADMIN_QUERY = "DELETE FROM admins where id = ?;";
     private static final String FIND_ALL_ADMINS_QUERY = "SELECT * FROM admins;";
     private static final String READ_ADMIN_QUERY = "SELECT * from admins where id = ?;";
+
+    private static final String EXISTS_ADMIN_QUERY = "SELECT EXISTS(SELECT * from admins WHERE id=?) AS result;";
     private static final String READ_ADMIN_BY_EMAIL_QUERY = "SELECT * from admins where email = ?;";
     private static final String UPDATE_ADMIN_QUERY = "UPDATE admins SET first_name = ?, last_name = ?, email = ?, " +
             "password = ?, superadmin = ?, enable = ? WHERE id = ?;";
@@ -66,6 +68,21 @@ public class AdminDao {
             e.printStackTrace();
         }
         return admin;
+    }
+
+    public boolean authorize(Integer adminId) {
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(EXISTS_ADMIN_QUERY)) {
+            statement.setInt(1, adminId);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getBoolean("result");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     public List<Admin> findAll() {
