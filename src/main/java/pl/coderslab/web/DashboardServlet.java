a83f7fb4e6498cb56bb889f57cba97492dedd7b0
MacApos <maciejapostol98@gmail.com>
2023-08-31 20:44:23 +0200

    5-strona-glowna

--- src/main/java/pl/coderslab/web/DashboardServlet.java
+++ src/main/java/pl/coderslab/web/DashboardServlet.java
@@ -1,5 +1,10 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.AdminDao;
+import pl.coderslab.dao.PlanDao;
+import pl.coderslab.dao.RecipeDao;
+import pl.coderslab.model.Admin;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,11 +14,39 @@ import java.io.IOException;
 public class DashboardServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/dashboard.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+        AdminDao adminDao = new AdminDao();
+        PlanDao planDao = new PlanDao();
+        RecipeDao recipeDao = new RecipeDao();
+
+        Admin readAdmin = adminDao.read(1);
+        readAdmin.setPassword("maslo");
+        adminDao.update(readAdmin);
+
+//        Admin admin;
+//        if (session.getAttribute("user") == null) {
+////            response.sendRedirect("/login");
+//            return;
+//        } else {
+//            admin = (Admin) session.getAttribute("user");
+//        }
+
+        response.getWriter().append("test");
+
+
+        int plansCount = planDao.getRecipesCountByAdmin(1);
+        int recipeCount = recipeDao.countRecipeByAdmin(1);
+
+        response.getWriter().append(plansCount + " " + recipeCount);
+
+
+//        getServletContext().getRequestDispatcher("/dashboard.jsp")
+//                .forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 
     }
 }
