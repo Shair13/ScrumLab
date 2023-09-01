897ae90a5744f0406e1a1c92928fa83a474bf655
MacApos <maciejapostol98@gmail.com>
2023-09-01 06:28:03 +0200

    5-Widok-strona-glowna

--- src/main/java/pl/coderslab/web/DashboardServlet.java
+++ src/main/java/pl/coderslab/web/DashboardServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "DashboardServlet", value = "/dashboard")
+@WebServlet(name = "DashboardServlet", value = "/app/dashboard")
 public class DashboardServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
