390c8d8995be9dceac7be4494759eb04fd8eb64e
Szer13 <shad350@gmail.com>
2023-09-01 17:23:09 +0200

    login validation

---
+++ src/main/webapp/js/loginValidation.js
@@ -0,0 +1,26 @@
+const formLogin = document.querySelector('form');
+const email = document.querySelector('#email');
+const password = document.querySelector("#password");
+const errorMessage = document.querySelector('div#error-message');
+
+formLogin.addEventListener('submit', onFormSubmission);
+
+function onFormSubmission(event) {
+
+    const errors = [];
+
+    if (!email.value.includes('@')) {
+        errors.push('Email musi posiadać znak @');
+    }
+    if (password.value === ""){
+        errors.push("Podaj hasło");
+    }
+
+    if (errors.length === 0) {
+        errorMessage.classList.add("d-none");
+    } else {
+        errorMessage.innerText = errors.join(", ");
+        errorMessage.classList.remove("d-none");
+        event.preventDefault();
+    }
+}
